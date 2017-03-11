5bb475a583021b2b1d99ba8d0a039062d2a10ae5
Dominik Horn <horn.dominik101@yahoo.de>
2017-03-11 14:41:12 +0100

    Eclipse Project setup

---
+++ src/de/gocodinggroup/kinectdatarecorder/main/Main.java
@@ -0,0 +1,7 @@
+package de.gocodinggroup.kinectdatarecorder.main;
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/de/gocodinggroup/kinectdatarecorder/main/Recorder.java
@@ -0,0 +1,5 @@
+package de.gocodinggroup.kinectdatarecorder.main;
+
+public class Recorder {
+
+}
